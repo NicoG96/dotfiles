a0fe8520e93a377f637ccaa395cc0c25272e9a1e
nico <797224+NicoG96@users.noreply.github.com>
2023-07-03 19:01:56 -0400

    Fix whitespace

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
 	set -Ux fish_greeting
 	set -Ux MACHINE_NAME (string split -m1 . $hostname)[1]
-    source $FISH_HOME/env/$MACHINE_NAME.fish
+	source $FISH_HOME/env/$MACHINE_NAME.fish
 end
