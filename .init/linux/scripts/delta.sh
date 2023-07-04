8246e22de80650f977fee3d61b44c52991bdde46
nico <797224+NicoG96@users.noreply.github.com>
2023-07-03 21:09:34 -0400

    Add -s to curl command

--- .init/linux/scripts/delta.sh
+++ .init/linux/scripts/delta.sh
@@ -10,7 +10,7 @@ VERSION=$(curl -s https://api.github.com/repos/dandavison/delta/releases/latest 
 PACKAGE_NAME="git-delta_${VERSION}_amd64.deb"
 ASSET_URL="${REPO_URL}/download/${VERSION}/${PACKAGE_NAME}"
 
-curl -L "$ASSET_URL" -o "$DOWNLOAD_DIR/${PACKAGE_NAME}"
+curl -sL "$ASSET_URL" -o "$DOWNLOAD_DIR/${PACKAGE_NAME}"
 
 echo "Installing delta..."
 sudo dpkg -i $HOME/Downloads/${PACKAGE_NAME}
