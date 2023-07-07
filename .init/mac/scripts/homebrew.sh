03f8b03fcc7260d0478057330c046b94d4a7fc67
nico <797224+NicoG96@users.noreply.github.com>
2023-07-06 21:17:00 -0400

    Add log

--- .init/mac/scripts/homebrew.sh
+++ .init/mac/scripts/homebrew.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 source ~/.init/common/utils/functions.sh
-print_section "Installing Homebrew..."
 
+print_section "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/$USER/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 source /Users/$USER/.zprofile
 
+print_section "Installing Homebrew formulae and casks..."
 brew update
 brew upgrade
 brew bundle install --file=~/Brewfile
