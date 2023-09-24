3dc2dd4c7888afe4f14653bd6b96e220221d359d
nico <797224+NicoG96@users.noreply.github.com>
2023-09-24 19:06:13 -0400

    Update shell startup commands with ssh-agent and error catching

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,16 @@
 if status is-interactive
 	set -Ux fish_greeting
 	set -Ux MACHINE_NAME (string split -m1 . $hostname)[1]
-	source $FISH_HOME/env/$MACHINE_NAME.fish
+
+	if test -e $FISH_HOME/env/$MACHINE_NAME.fish
+		source $FISH_HOME/env/$MACHINE_NAME.fish
+	else
+		echo "Error sourcing Fish environment file: $FISH_HOME/env/$MACHINE_NAME.fish not found!"
+	end
+
+	if test -e ~/.ssh/id_ed25519
+		ssh-add ~/.ssh/id_ed25519
+	else
+		echo "Error adding private key to ssh-agent: file id_ed25519 not found!"
+	end
 end
