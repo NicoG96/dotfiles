a00c65d6bc0bac07f0bdeb1e2cf0de556df1f44b
nico <797224+NicoG96@users.noreply.github.com>
2024-04-04 12:03:21 -0400

    Add additional install instructions

--- .init/linux/scripts/bat-extras.sh
+++ .init/linux/scripts/bat-extras.sh
@@ -3,5 +3,6 @@
 source ~/.init/common/utils/functions.sh
 
 print_section "Installing bat-extras..."
-git clone git@github.com:eth-p/bat-extras.git $HOME/utils
+mkdir -p $HOME/utils/bat-extras
+git clone git@github.com:eth-p/bat-extras.git $HOME/utils/bat-extras
 sudo $HOME/utils/bat-extras/build.sh --install
